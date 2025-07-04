09f10459b4f92ed3bdd4a8e7ea54e897537f85ba
Rui Miguel <rui_miguel041994@hotmail.com>
2025-07-04 16:11:50 +0100

    added get all movies and delete maps + moved to another class

---
+++ Endpoints/MoviesEndpoints.cs
@@ -0,0 +1,74 @@
+using MoviesAspNetCore.DTOs;
+using MoviesAspNetCore.Entities;
+
+namespace MoviesAspNetCore.Endpoints;
+
+public static class MoviesEndpoints
+{
+    private const string MOVIES_ENDPOINT = "movies";
+    private const string MOVIE_WITH_NAME = "GetMovie";
+    private static List<Movie> _movies = new List<Movie>();
+    
+    public static RouteGroupBuilder MapMoviesEndpoints(this WebApplication application)
+    {
+        var group = application.MapGroup(MOVIES_ENDPOINT).WithParameterValidation();
+
+        group.MapGet("/", () => Results.Ok(_movies));
+        
+        group.MapGet("/{id}", (int id) =>
+        {
+            var movie = _movies.FirstOrDefault(m => m.Id == id);
+            return movie is null ? Results.NotFound() : Results.Ok(movie);
+        }).WithName(MOVIE_WITH_NAME);
+
+        group.MapPost("/", (CreateMovieDTO movie) =>
+        {
+            Movie movieDBO = new Movie()
+            {
+                Id = _movies.Count + 1,
+                Title = movie.Title,
+                Description = movie.Description,
+                Genre = movie.Genre,
+                ReleaseDate = movie.ReleaseDate,
+                Actors = movie.Actors
+            };
+
+            _movies.Add(movieDBO);
+
+            return Results.CreatedAtRoute(MOVIE_WITH_NAME, new { id = movieDBO.Id }, movieDBO);
+        });
+
+        group.MapPut("/{id}", (int id, UpdateMovieDTO movie) =>
+        {
+            Movie movieDBO = _movies.FirstOrDefault(m => m.Id == id);
+
+            if (movieDBO is null)
+            {
+                return Results.NotFound();
+            }
+
+            movieDBO.Title = movie.Title;
+            movieDBO.Description = movie.Description;
+            movieDBO.Genre = movie.Genre;
+            movieDBO.ReleaseDate = movie.ReleaseDate;
+            movieDBO.Actors = movie.Actors;
+            
+            return Results.NoContent();
+        });
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            Movie movieDBO = _movies.FirstOrDefault(m => m.Id == id);
+            
+            if (movieDBO is null)
+            {
+                return Results.NotFound();
+            }
+
+            _movies.Remove(movieDBO);
+            return Results.NoContent();
+        });
+
+        return group;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,32 +1,10 @@
 using MoviesAspNetCore.DTOs;
+using MoviesAspNetCore.Endpoints;
 using MoviesAspNetCore.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Movie> movies = new List<Movie>();
-
-app.MapGet("/movies/{id}", (int id) =>
-{
-    var movie = movies.FirstOrDefault(m => m.Id == id);
-    return movie is null ? Results.NotFound() : Results.Ok(movie);
-}).WithName("GetMovie");
-
-app.MapPost("/movies", (CreateMovieDTO movie) =>
-{
-    Movie movieDBO = new Movie()
-    {
-        Id = movies.Count + 1,
-        Title = movie.Title,
-        Description = movie.Description,
-        Genre = movie.Genre,
-        ReleaseDate = movie.ReleaseDate,
-        Actors = movie.Actors
-    };
-
-    movies.Add(movieDBO);
-
-    return Results.CreatedAtRoute("GetMovie", new { id = movieDBO.Id }, movieDBO);
-}).WithParameterValidation();
+app.MapMoviesEndpoints();
 
 app.Run();
